51f4d605376c6a7a7c91b8e2088339e743bc0b3c
IMRANDIL <aliimranadil2@gmail.com>
2022-01-31 10:45:50 +0530

    yh

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const searchInput = document.querySelector('[data-search]');
 let users = [];
 
 searchInput.addEventListener('input', (e) => {
-    const value = e.target.value;
+    const value = e.target.value.toLowerCase();
     users.forEach((user) => {
-        const isVisible = user.name.includes(value) || user.email.includes(value);
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
         user.element.classList.toggle('hide', !isVisible)
     })
 })
@@ -18,14 +18,14 @@ searchInput.addEventListener('input', (e) => {
 
 
 fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json()).then((data) => {
-    users = data.map((user) => {
+    users = data.map(({ name, email }) => {
         const card = userCardTemplate.content.cloneNode(true).children[0];
         const header = card.querySelector('[data-header]');
         const body = card.querySelector('[data-body]');
-        header.textContent = user.name;
-        body.textContent = user.email;
+        header.textContent = name;
+        body.textContent = email;
         userCardContainer.append(card);
-        return { name: user.name, email: user.email, element: card }
+        return { name: name, email: email, element: card }
     })
 
 }).catch(err => console.log(err))
